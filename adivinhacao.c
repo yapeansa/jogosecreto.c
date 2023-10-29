b2650bbdf4772a2c8946d8e1c125d58ac6c49a1a
yapeansa <yago.pereira@estudante.ufjf.br>
2023-10-29 10:55:51 -0300

    Avançando no jogo número secreto.

--- adivinhacao.c
+++ adivinhacao.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#define NUMERO_DE_TENTATIVAS 5
 
 int main()
 {
@@ -9,30 +10,50 @@ int main()
 
     int numerosecreto = 42;
     int chute;
+    int ganhou = 0;
+    int tentativas = 1;
 
-    printf("Qual é o seu chute: ");
-    scanf("%d", &chute);
-    printf("Seu chute foi: %d.\n", chute);
-
-    int acertou = (chute == numerosecreto);
-
-    if (acertou)
+    while (!ganhou)
     {
-        printf("Parabéns. Você acertou!\n");
-        printf("Jogue de novo, você é um bom jogador!\n");
-    }
-    else
-    {
-        printf("Você errou!\n");
-        printf("Mas não desanime, tente novamente!\n");
-        if (numerosecreto > chute)
+        printf("Tentativa %d de %d.\n", tentativas, NUMERO_DE_TENTATIVAS);
+        printf("Qual é o seu chute: ");
+        scanf("%d", &chute);
+        printf("Seu chute foi: %d.\n", chute);
+
+        int acertou = (chute == numerosecreto);
+        int maior = (chute > numerosecreto);
+
+        if (chute < 0)
         {
-            printf("O número secreto é menor do que %d.\n", chute);
+            printf("Você não pode chutar um número negativo.");
+            continue;
+        }
+        else if (acertou)
+        {
+            printf("Parabéns. Você acertou!\n");
+            printf("Jogue de novo, você é um bom jogador!\n");
+            printf("Fim de jogo!\n");
+            ganhou = 1;
+            break;
         }
         else
         {
-            printf("O número secreto é maior do que %d.\n", chute);
+            printf("Você errou!\n");
+            printf("Mas não desanime, tente novamente!\n");
+            if (maior)
+            {
+                printf("Seu chute foi maior do que o número secreto.\n");
+            }
+            else
+            {
+                printf("Seu chute foi menor do que o número secreto.\n");
+            }
         }
+        if (tentativas == 5)
+        {
+            break;
+        }
+        tentativas += 1;
     }
 
     return 0;
